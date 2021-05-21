b1adfe0fd2d04b7354bc94e59c2f5977ac5e06f4
Divanshu Agarwal <divanshuagarwal2001@gmail.com>
2021-05-22 02:36:25 +0530

    login signup init

---
+++ app/src/main/java/com/example/conduit/ui/auth/SignupFragment.kt
@@ -0,0 +1,21 @@
+package com.example.conduit.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.conduit.databinding.FragmentSignupBinding
+
+class SignupFragment : Fragment() {
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
